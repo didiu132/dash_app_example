38a5822dd1cbe86cdb6ee4d8130a9f3074397013
didiu132 <58175898+didiu132@users.noreply.github.com>
2019-11-25 15:18:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@
 # In[ ]:
 
 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
 app = dash.Dash(__name__)
 server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
